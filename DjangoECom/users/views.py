2eab86f60b38484153f0327654b9a181d2ab52fa
Mohamed Tarek <mohamedtarek251999@gmail.com>
2020-12-05 21:35:08 +0200

    Add register customer

--- DjangoECom/users/views.py
+++ DjangoECom/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import redirect
+from .models import Customer,Seller
 
 # Create your views here.
 def index(request):
@@ -17,18 +18,35 @@ def account_view(request):
             username = request.POST["username"]
             password = request.POST["password"]
             user = authenticate(request, username=username, password=password)
-         if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-         else:
-            return render(request, "users/login.html", {
+             if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request, "users/login.html", {
                 "message": "Invalid credentials."
-            })
-         return render(request, "users/login.html")
+                })
 
-    elif request.POST.get('submit') == 'sign_up':
-            # your sign up logic goes here
+        elif request.POST.get('submit') == 'sign_up':
+                # your sign up logic goes here
+            firstname=request.POST["firstname"]
+            secondname=request.POST["secondname"]
+            username = request.POST["username"]
+            password1 = request.POST["password1"]
+            password2 =request.POST["password2"]
+            email=request.POST["email"]
+            phone=request.POST["phone"]
+            address=request.POST["address"]
 
+            if(password1 == password2):
+                customer=Customer.objects.create(username=username,password=password1,phone=phone,address=address,email=email,firstname=firstname,secondname=secondname)
+                customer.save()
+                login(request,customer)
+            else:
+                return render(request, "users/login.html", {
+                    "message": "Password not match"
+                })
+
+    return render(request, "users/login.html")
 
 def logout(request):
     auth.logout(request)
