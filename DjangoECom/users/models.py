d9416cc3d1c4ec87d8f5a75efad8f2d9f7a01a45
Mostafa Samy <mostafa.elsahy1@gamil.com>
2020-12-07 07:00:06 +0200

    Last Commit before fixing login

--- DjangoECom/users/models.py
+++ DjangoECom/users/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-
 # Create your models here.
 class Customer(models.Model):
-    firstname = models.CharField(max_length=10)
-    seconedname = models.CharField(max_length=10)
-    username = models.CharField(max_length=64, unique=True)
-    email = models.EmailField(max_length=128, unique=True)
-    password = models.CharField(max_length=64)
-    phone = models.IntegerField(max_length=12)
+    firstname = models.CharField(max_length=10,null=True)
+    secondname = models.CharField(max_length=10,null=True)
+    username = models.CharField(max_length=64, unique=True,null=True)
+    email = models.EmailField(max_length=128, unique=True,null=True)
+    password = models.CharField(max_length=64,null=True)
+    phone = models.IntegerField(null=True)
 
 class Seller(models.Model):
-    firstname = models.CharField(max_length=10)
-    seconedname = models.CharField(max_length=10)
-    username = models.CharField(max_length=64, unique=True)
-    email = models.EmailField(max_length=128, unique=True)
-    password = models.CharField(max_length=64)
-    phone = models.IntegerField(max_length=12)
+    firstname = models.CharField(max_length=10,null=True)
+    secondname = models.CharField(max_length=10,null=True)
+    username = models.CharField(max_length=64, unique=True,null=True)
+    email = models.EmailField(max_length=128, unique=True,null=True)
+    password = models.CharField(max_length=64,null=True)
+    phone = models.IntegerField(null=True)
--- DjangoECom/users/views.py
+++ DjangoECom/users/views.py
@@ -3,50 +3,68 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import redirect
-from .models import Customer,Seller
+from .models import Customer, Seller
+
 
 # Create your views here.
+
 def index(request):
     return render(request, "users/index.html")
 
 
-    
 def account_view(request):
     if request.method == "POST":
-        if request.POST.get('submit') == 'login':
+        if request.POST['regSubmitBtn'] == 'login':
             # your sign in logic goes here
             username = request.POST["logUserName"]
             password = request.POST["logPassword"]
-            check = request.Post["loguser"]
+            check = request.POST["loguser"]
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return HttpResponseRedirect(reverse("index"))
             else:
                 return render(request, "users/account.html", {
-                "message": "Invalid credentials."
+                    "message": "Invalid credentials."
                 })
 
-        elif request.POST.get('submit') == 'signup':
-                # your sign up logic goes here
-            firstname=request.POST["regFirstName"]
-            secondname=request.POST["regLastName"]
+        elif request.POST['regSubmitBtn'] == 'signup':
+            # your sign up logic goes here
+            firstname = request.POST["regFirstName"]
+            secondname = request.POST["regLastName"]
             username = request.POST["regUserName"]
             password1 = request.POST["regPassword"]
-            password2 =request.POST["regReEnteredPassword"]
-            email=request.POST["regEmailAddress"]
-            phone=request.POST["regPhoneNumber"]
-            check = request.Post["reguser"]
+            password2 = request.POST["regReEnteredPassword"]
+            email = request.POST["regEmailAddress"]
+            phone = request.POST["regPhoneNumber"]
+            check = request.POST["reguser"]
 
-            if(password1 == password2):
-                customer=Customer.objects.create(username=username, password=password1, phone=phone, email=email, firstname=firstname, secondname=secondname)
-                customer.save()
-                login(request,customer)
+            if password1 == password2:
+                if check == "seller":
+                    seller = Seller.objects.create(username=username,
+                                    password=password1,
+                                    phone=phone,
+                                    email=email,
+                                    firstname=firstname,
+                                    secondname=secondname)
+                    seller.save()
+                    login(request, seller)
+                else:
+                    customer = Customer.objects.create(
+                        username=username,
+                        password=password1,
+                        phone=phone,
+                        email=email,
+                        firstname=firstname,
+                        secondname=secondname)
+                    customer.save()
+                    login(request, customer)
             else:
                 return render(request, "users/account.html", {
                     "message": "Password not match"
                 })
     return render(request, "users/account.html")
+
 
 def logout(request):
     logout(request)
