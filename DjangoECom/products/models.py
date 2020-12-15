09c8ce0ffe98ab67cdf71759ecdbe3805bc85255
mohamed-samy24 <73324284+mohamed-samy24@users.noreply.github.com>
2020-12-15 10:13:23 +0200

    added shipping address to model ,, productprocess to views,,cookiecart & cartdata & guestOrder

--- DjangoECom/products/models.py
+++ DjangoECom/products/models.py
@@ -87,4 +87,13 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    pass
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
