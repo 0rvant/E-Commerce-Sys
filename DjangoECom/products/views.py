a505b76460146b957473d3975d0b10d121d28849
mohamed-samy2499 <1701198@eng.asu.edu.eg>
2020-12-25 03:34:38 +0200

    checkout views and frontend//order button still didn't merge

--- DjangoECom/products/views.py
+++ DjangoECom/products/views.py
@@ -71,19 +71,14 @@ def cart(request):
 
 @allowed_users(allowed_roles=['customer'])
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order,created = Order.objects.get_or_create(customer=customer,complete=False)
-        items =order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items=[]
-        order ={'get_cart_total':0,'get_cart_items':0}
-        cartItems = order['get_cart_items']
+    data = cartData(request)
 
-    context ={'items':items , 'order':order,'cartItems':cartItems}
-    return render(request, 'products/cart.html',context)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    return render(request, 'store/checkout.html', context)
 def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
